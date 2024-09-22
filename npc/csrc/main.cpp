46be23516266ee9f378fe273ed74d006fec115ec
tracer-ysyx <tracer@ysyx.org>
2024-09-22 12:17:16 +0800

    > compile NEMU ysyx_20240908 罗权 Linux lq-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:17:16 up  1:46,  1 user,  load average: 0.44, 0.57, 0.28

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,48 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-//#include <nvboard.h>
+#include <nvboard.h>
 
 #include "Vtop.h"
-#include "verilated.h"
-//#include "verilated_vcd_c.h"
-#include "verilated_fst_c.h"
 
-// static TOP_NAME dut;
+static TOP_NAME dut;
 
-// void nvboard_bind_all_pins(TOP_NAME* top);
+void nvboard_bind_all_pins(TOP_NAME* top);
 
 
 int main(int argc, char** argv) {
-    VerilatedContext* contextp = new VerilatedContext;
-    contextp->commandArgs(argc, argv);
-    Vtop* top = new Vtop{contextp};
-
-    /* generate VCD 格式 */
-    // VerilatedVcdC* tfp = new VerilatedVcdC;
-    /* generate FST 格式 */
-    VerilatedFstC* tfp = new VerilatedFstC;
-    contextp->traceEverOn(true);
-    top->trace(tfp, 0); // 0 是自定义的起始时间
-    tfp->open("simx.fst");
     
-    // nvboard_bind_all_pins(&dut);
-    // nvboard_init();
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
 
     while (1) {
-    	// nvboard_update();
-        contextp->timeInc(1);
-        int a = rand() & 1;
-        int b = rand() & 1;
-        top->a = a;
-        top->b = b;
-        top->eval();
-        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-        assert(top->f == (a ^ b));
-        tfp->dump(contextp->time());
+    	nvboard_update();
+        dut.eval();
+        printf("a = %d, b = %d, f = %d\n", dut.a, dut.b, dut.f);
     }
-    delete top;
-    tfp->close();
-    delete contextp;
     return 0;
 }
