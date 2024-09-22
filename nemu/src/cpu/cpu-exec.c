22dcc1ba8b1f66ad4418b21bbfe282e18748b378
tracer-ysyx <tracer@ysyx.org>
2024-09-22 21:40:25 +0800

    > compile NEMU ysyx_20240908 罗权 Linux lq-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:40:25 up  5:51,  1 user,  load average: 0.66, 0.46, 0.28

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -69,6 +69,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 
 static void execute(uint64_t n) {
   Decode s;
+  printf("n : %ld,asdasd\n",n);
   for (;n > 0; n --) {
     printf("n : %ld\n",n);
     exec_once(&s, cpu.pc);
