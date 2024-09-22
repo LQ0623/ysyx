77260a2134180e9a3a8763a2eb32133b37803a3f
LUO QUAN <2990261695@qq.com>
2024-09-22 11:16:30 +0800

    before starting pa1

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <nvboard.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+#include "Vtop.h"
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+
+int main(int argc, char** argv) {
+    
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+
+    while (1) {
+    	nvboard_update();
+        dut.eval();
+        printf("a = %d, b = %d, f = %d\n", dut.a, dut.b, dut.f);
+    }
+    return 0;
 }
