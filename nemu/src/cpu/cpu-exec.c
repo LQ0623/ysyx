5d30eb9753d1d8a32d2b7425b724b443f7a9dcc9
tracer-ysyx <tracer@ysyx.org>
2024-09-23 09:29:36 +0800

    > compile NEMU ysyx_20240908 罗权 Linux lq-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:29:36 up 4 min,  1 user,  load average: 0.06, 0.26, 0.14

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -95,7 +95,7 @@ void assert_fail_msg() {
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
   g_print_step = (n < MAX_INST_TO_PRINT);
-  printf("nemu_state.state:%d\n",nemu_state.state);
+  // printf("nemu_state.state:%d\n",nemu_state.state);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT: case NEMU_QUIT:
       printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
@@ -107,7 +107,7 @@ void cpu_exec(uint64_t n) {
 
   execute(n);
 
-  printf("nemu_state.state:%d\n",nemu_state.state);
+  // printf("nemu_state.state:%d\n",nemu_state.state);
   uint64_t timer_end = get_time();
   g_timer += timer_end - timer_start;
 
