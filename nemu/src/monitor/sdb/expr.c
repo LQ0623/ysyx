7f2ddf6366249bfe9548a4cdbb5d949bdaf9aa1d
tracer-ysyx <tracer@ysyx.org>
2024-09-25 15:20:46 +0800

    > compile NEMU ysyx_20240908 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:20:46 up  2:19,  1 user,  load average: 0.06, 0.10, 0.20

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -119,10 +119,10 @@ static bool make_token(char *e) {
           case TK_NUM:
             tokens[nr_token].type = rules[i].token_type;
             strncpy(tokens[nr_token].str, substr_start, substr_len);
-            //tokens[nr_token].str[substr_len-1] = '\0'; // 确保字符串以空字符结尾
+            tokens[nr_token].str[substr_len] = '\0'; // 确保字符串以空字符结尾
             nr_token++;
             break;
-          default: TODO();
+          default: panic("cannot split token");
         }
 
         break;
