70526431f53481881107804752ea90f2658845b9
tracer-ysyx <tracer@ysyx.org>
2024-09-25 16:28:35 +0800

    > compile NEMU ysyx_20240908 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:28:35 up  3:27,  1 user,  load average: 1.16, 0.61, 0.30

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -224,6 +224,7 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   // TODO();
   int val = eval(0,nr_token-1);
+  *success = true;
   printf("%d\n",val);
 
   return 0;
