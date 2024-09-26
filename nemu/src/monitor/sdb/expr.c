a7a9c2b6cdd0ee0b2b4c522385352b7353f1a03d
tracer-ysyx <tracer@ysyx.org>
2024-09-26 14:09:53 +0800

    > compile NEMU ysyx_20240908 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:09:53 up  4:40,  1 user,  load average: 0.31, 0.09, 0.06

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -47,7 +47,7 @@ static struct rule {
   {"\\)", TK_RIGHT},           // )
   {"[0-9]+", TK_DEC},       // 0-9
   {"\\$[0-9,a-z]+", TK_REG},      // REG
-  {"0[x,X][0-9,a-f,A-F]+", TK_HEX},   // 0-f
+  {"[0x,0X][0-9,a-f,A-F]+", TK_HEX},   // 0-f
   {"!=", TK_NEQ},            // not equal
   {"&&", TK_AND},            // AND
   {"<=", TK_LESS_EQ},           // less than   
