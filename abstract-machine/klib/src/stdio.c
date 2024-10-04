b11aa3f64952669f797c9ab2beaed061612673b1
tracer-ysyx <tracer@ysyx.org>
2024-10-04 17:56:55 +0800

    > compile NEMU ysyx_20240908 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:56:55 up  4:45,  1 user,  load average: 0.31, 0.27, 0.24

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -15,49 +15,37 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 int sprintf(char *out, const char *fmt, ...) {
   //panic("Not implemented");
-    va_list args;
-    va_start(args, fmt);
-
-    int index = 0;
-    int result = 0;
-
-    while (*fmt != '\0') {
-        if (*fmt == '%') {
-            fmt++;
-            switch (*fmt) {
-                case 'd': {
-                    int i = va_arg(args, int);
-                    char buffer[12]; // 足够大以容纳int的最大值（包括负号）
-                    int len = sprintf(buffer, "%d", i);
-                    for (int i = 0; i < len; i++) {
-                        out[index++] = buffer[i];
-                    }
-                    result += len;
-                    break;
-                }
-                case 's': {
-                    char *s = va_arg(args, char*);
-                    size_t s_len = strlen(s);
-                    for (size_t i = 0; i < s_len; i++) {
-                        out[index++] = s[i];
-                    }
-                    result += s_len;
-                    break;
-                }
-                default:
-                        out[index++] = *fmt;
-                        result++;
-                    	break;
-            }
-        } else {
-                out[index++] = *fmt;
-                result++;
-        }
-        fmt++;
-    }
-
-    va_end(args);
-    return result;
+  va_list args;
+  va_start(args, fmt);
+  
+  int index = 0;
+  
+  while(*fmt != '\0'){
+  	if(*fmt == '%'){
+  		fmt++;
+  		switch(*fmt){
+  			case 'd':
+  				int i = va_arg(args, int);
+  				out[index++] = i;
+  				break;
+  			case 's':
+  				char *s = va_arg(args, char*);
+  				for(size_t i = 0;i < strlen(s);i++){
+  					out[index++] = s[i];
+  				}
+  				break;
+  			default:
+  				out[index++] = *fmt;
+  				
+  		}
+  	} else{
+  		out[index++] = *fmt;
+  	}
+  	fmt++;
+  }
+  
+  va_end(args);
+  return 0;
 
 }
 
