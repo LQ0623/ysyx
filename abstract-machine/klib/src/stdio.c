4cc1011376d035dbc55ead51635727b82b0913f6
tracer-ysyx <tracer@ysyx.org>
2024-10-04 17:59:36 +0800

    > compile NEMU ysyx_20240908 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:59:36 up  4:47,  1 user,  load average: 0.06, 0.19, 0.21

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -26,7 +26,12 @@ int sprintf(char *out, const char *fmt, ...) {
   		switch(*fmt){
   			case 'd':
   				int i = va_arg(args, int);
-  				out[index++] = i;
+  				int tmp;
+  				while(i>=0){
+  					tmp = i%10;
+  					out[index++] = tmp + '0';
+  					i = i/10;
+  				}
   				break;
   			case 's':
   				char *s = va_arg(args, char*);
