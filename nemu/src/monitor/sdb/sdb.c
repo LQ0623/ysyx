dd587da5ae245f4b02c0a15f53318ce5a9723ad8
tracer-ysyx <tracer@ysyx.org>
2024-10-05 10:50:37 +0800

    > compile NEMU ysyx_20240908 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:50:37 up  1:37,  1 user,  load average: 0.51, 0.24, 0.19

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,7 +55,7 @@ static int cmd_c(char *args) {
 static int cmd_q(char *args) {
   // cmd_q 函数只是简单的返回-1,但是并没有实现完整的退出逻辑
   //exit(0); // 使用 exit(0) 退出程序
-  //nemu_state.state = NEMU_QUIT;
+  nemu_state.state = NEMU_QUIT;
   return -1;
 }
 
