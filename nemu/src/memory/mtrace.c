3f7665b2137fb95f8fd400a8cf73e963dd2ff4fe
tracer-ysyx <tracer@ysyx.org>
2024-10-08 21:42:32 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:42:32 up 11:00,  1 user,  load average: 0.63, 0.63, 0.55

--- nemu/src/memory/mtrace.c
+++ nemu/src/memory/mtrace.c
@@ -3,15 +3,23 @@
 char file_path[] = "../mtrace.log"; 
 
 void init_mtrace_log(){
+
+#if CONFIG_MTRACE
+
     FILE *file = fopen(file_path, "w");
     if(file == NULL){
         panic("打开或创建文件时出错");
     }
 
     fclose(file);
+#endif
+
 }
 
 void mtrace_log_write(paddr_t addr, int len, char operate,word_t data){
+
+#if CONFIG_MTRACE
+
     FILE *file = fopen(file_path, "a+");
     if(file == NULL){
         panic("打开文件时出错");
@@ -23,4 +31,7 @@ void mtrace_log_write(paddr_t addr, int len, char operate,word_t data){
         fprintf(file, "This has wrote addr : 0x%x  len : %d,the data is %x\n", addr, len, data);
     }
     fclose(file);
+
+#endif
+
 }
