986e802fbf14cd4167f4c1c7a7dfb1937fc3c72f
tracer-ysyx <tracer@ysyx.org>
2024-10-11 17:12:23 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:12:23 up  3:03,  1 user,  load average: 0.32, 0.25, 0.34

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -18,23 +18,23 @@ void init_ftrace(char* elf_file){
  *      函数的地址
  */
 void ftrace_function(char operate,vaddr_t addr_inv,vaddr_t addr_func){
-    printf("0x%x:\n",addr_inv);
+    printf("0x%x:",addr_inv);
     if(operate == 'c'){
         // for(int i = 0;i < count_inv;i++){
         //     printf("  ");
         // }
-        printf("call [\n");
-        char* str;
-        for(int i = 0;i < MAX_FUNC;i++){
+        printf("call [");
+        // char* str;
+        int i;
+        for(i = 0;i < MAX_FUNC;i++){
             if(func_name[i].addr == addr_func){
-                str = func_name[i].name;
                 break;
             }
         }
-        if(str == NULL){
+        if(i == MAX_FUNC){
             panic("解析错误");
         }
-        printf("%s@0x%x]\n",str,addr_func);
+        printf("%s@0x%x]    count_inv:%d\n",func_name[i].name,addr_func,count_inv);
         count_inv++;
     }else if(operate == 'r'){
         for(int i = 0;i < count_inv;i++){
