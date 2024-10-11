7c00fce5f9c105d7b3484b6e8670dda14b2595cf
tracer-ysyx <tracer@ysyx.org>
2024-10-11 17:15:05 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:15:05 up  3:06,  1 user,  load average: 0.53, 0.29, 0.33

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -35,7 +35,7 @@ void ftrace_function(char operate,vaddr_t addr_inv,vaddr_t addr_func){
         if(str == NULL){
             panic("解析错误");
         }
-        printf("%s@0x%x]        %s  %d\n",str,addr_func,func_name[i].name,count_inv);
+        printf("%s@0x%x]          %d\n",str,addr_func,count_inv);
         count_inv++;
     }else if(operate == 'r'){
         for(int i = 0;i < count_inv;i++){
