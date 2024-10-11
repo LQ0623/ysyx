3af84adcafc2104f5fe7c1e8b46f98a3bca5feb8
tracer-ysyx <tracer@ysyx.org>
2024-10-11 18:41:09 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:41:09 up  4:32,  1 user,  load average: 0.08, 0.03, 0.07

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -97,6 +97,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf=FILE           elf file to be parsed\n");
         printf("\n");
         exit(0);
     }
