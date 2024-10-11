d4f3c5465b0c09fcfaf80ee1e2f822f31757e306
tracer-ysyx <tracer@ysyx.org>
2024-10-11 17:09:24 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:09:24 up  3:00,  1 user,  load average: 0.11, 0.25, 0.36

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -18,12 +18,12 @@ void init_ftrace(char* elf_file){
  *      函数的地址
  */
 void ftrace_function(char operate,vaddr_t addr_inv,vaddr_t addr_func){
-    printf("0x%x:",addr_inv);
+    printf("0x%x:\n",addr_inv);
     if(operate == 'c'){
         // for(int i = 0;i < count_inv;i++){
         //     printf("  ");
         // }
-        printf("call [");
+        printf("call [\n");
         char* str;
         for(int i = 0;i < MAX_FUNC;i++){
             if(func_name[i].addr == addr_func){
