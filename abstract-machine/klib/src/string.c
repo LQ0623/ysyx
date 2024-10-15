440180fcec265323164328da0050480f3feea4df
tracer-ysyx <tracer@ysyx.org>
2024-10-15 20:43:56 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:43:56 up  1:10,  1 user,  load average: 0.08, 0.25, 0.34

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -93,18 +93,21 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  char *char_dst = (char *)dst;
-  char *char_src = (char *)src;
+    //panic("Not implemented");
+    char *d = (char *)dst;
+    const char *s = (const char *)src;
 
-  if (dst < src){
-    for (int i = 0; i < n; ++i)
-      char_dst[i] = char_src[i];
-  }else if(dst > src){
-    for (int i = n-1; i >= 0; --i)
-      char_dst[i] = char_src[i];
-  }//else 不需要复制
+    if (d < s) {
+        for (int i = 0; i < n; i++) {
+            d[i] = s[i];
+        }
+    } else if (d > s) {
+        for (int i = n - 1; i >= 0; i--) {
+            d[i] = s[i];
+        }
+    }
 
-  return dst;
+    return dst;;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
