b3e4089443ad9ed78ce3d155e0dd5c0389792828
tracer-ysyx <tracer@ysyx.org>
2024-10-15 20:48:28 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:48:28 up  1:15,  1 user,  load average: 0.41, 0.34, 0.35

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -22,7 +22,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int reg_num = ARRLEN(cpu.gpr);
   for(int i = 0; i < reg_num;i++){
     if(cpu.gpr[i] != ref_r->gpr[i]){
-      printf("error in %d\n",i);
+      panic("error in %d\n",i);
       return false;
     }
   }
