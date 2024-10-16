49ae9ff0fdcb2541eab67391e00c6554f00597dd
tracer-ysyx <tracer@ysyx.org>
2024-10-16 19:28:02 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:28:02 up  5:18,  1 user,  load average: 0.13, 0.13, 0.16

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -4,10 +4,14 @@
 void __am_timer_init() {
 }
 
+// AM系统启动时间, 可读出系统启动后的微秒数.
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uint32_t low_part = inl(RTC_ADDR);
+  uint32_t high_part = inl(RTC_ADDR + 4);
+  uptime->us = ((uint64_t)high_part) << 32 | low_part;
 }
 
+//  AM实时时钟(RTC, Real Time Clock), 可读出当前的年月日时分秒.
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
