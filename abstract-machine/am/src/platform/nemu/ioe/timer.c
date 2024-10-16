8b6123811ba603ad9e9a64fe8414d3b3952ee07d
tracer-ysyx <tracer@ysyx.org>
2024-10-16 20:14:45 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:14:45 up  6:05,  1 user,  load average: 0.25, 0.24, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,8 +6,8 @@ void __am_timer_init() {
 
 // AM系统启动时间, 可读出系统启动后的微秒数.
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint32_t low_part = inl(RTC_ADDR);
   uint32_t high_part = inl(RTC_ADDR + 4);
+  uint32_t low_part = inl(RTC_ADDR);
   uptime->us = ((uint64_t)high_part) << 32 | low_part;
 }
 
