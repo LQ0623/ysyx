53a22ec3f967ec85dbd1df47bf4ce29ab112f218
tracer-ysyx <tracer@ysyx.org>
2024-10-16 15:33:19 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:33:19 up  1:23,  1 user,  load average: 1.20, 0.77, 0.40

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,173 +5,83 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-static char HEX_CHARACTERS[] = "0123456789ABCDEF";
+int vsprintf(char *out, const char *fmt, va_list ap);
 
 int printf(const char *fmt, ...) {
-//   panic("Not implemented");
+  char buffer[1024];
+  va_list arg;
+  va_start (arg, fmt);
+  
+  int done = vsprintf(buffer, fmt, arg);
 
-	int count;
-	char buf[2048];
-	va_list args;
-	// 解析...中的参数
-	va_start(args, fmt);
-	
-	count = vsprintf(buf, fmt, args);
+  putstr(buffer);
 
-	// for(int i = 0; i < count;i++){
-	// 	putch(buf[i]);
-	// }
-	putstr(buf);
-
-	va_end(args);
-	return count;
+  va_end(arg);
+  return done;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-//   panic("Not implemented");
+  char buffer[128];
+  char *txt, cha;
+  int num, len;
+  
+  int state = 0, i, j;//模仿一个状态机
+  for (i = 0, j = 0; fmt[i] != '\0'; ++i){
+    switch (state)
+    {
+    case 0:
+      if (fmt[i] != '%')
+        out[j++]=fmt[i];
+      else
+        state = 1;
+      break;
+    
+    case 1:
+      switch (fmt[i])
+      {
+      case 's':
+        txt = va_arg(ap, char*);
+        for (int k = 0; txt[k] !='\0'; ++k)
+          out[j++] = txt[k];
+        break;
+      
+      case 'd':
+        num = va_arg(ap, int);
+        if(num == 0){
+          out[j++] = '0';
+          break;
+        }
+        for (len = 0; num ; num /= 10, ++len)
+          buffer[len] = num % 10 + '0';//逆序的
+        for (int k = len - 1; k >= 0; --k)
+          out[j++] = buffer[k];
+        break;
+      
+      case 'c':
+        cha = (char)va_arg(ap, int);
+        out[j++] = cha;
+        break;
 
-	int index = 0;
+      default:
+        assert(0);
+      }
+      state = 0;
+      break;
+      
+    }
+  }
 
-	while(*fmt){
-		if(*fmt != '%'){
-			out[index++] = *fmt;
-		}else{
-			fmt++;
-			/**
-			 * width: 指定输出字段的最小宽度
-			 * precision: 控制输出的精度。
-			 * argc_type: 指定参数的类型。
-			 */
-			int width =0,precision = -1;
-			//char argc_type = 0;
-			while(*fmt>='0' && *fmt<='9'){
-				width = width * 10 + (*fmt - '0');
-				fmt ++;
-			}
-			if(*fmt == '.'){
-				fmt++;
-				precision = 0;
-				while(*fmt>='0' && *fmt<='9'){
-					precision = precision * 10 + (*fmt - '0');
-					fmt ++;
-				}
-			}
-			// if (*fmt == 'h' || *fmt == 'l' || *fmt == 'L') {
-            //     argc_type = *fmt++;
-            // }
-
-			switch(*fmt){
-				case 's':{
-					char *s = va_arg(ap, char*);
-					int len = strlen(s);
-					if (precision >= 0 && precision < len) {
-                        len = precision;
-                    }
-					if (width > len) {
-                        for (int i = 0; i < width - len; i++) {
-                            out[index++] = ' '; // 右对齐
-                        }
-                    }
-					strncpy(out + index, s, len);
-					index += len;
-					break;
-				}
-				case 'd':{
-					int i = va_arg(ap, int);
-					if(i == 0){
-						out[index++] = '0';
-					}else {
-						if(i < 0){
-							out[index++] = '-';
-							i = 0 - i;
-						}
-						int digital;
-						char temp[32];
-						int count = 0;
-						while(i>0){
-							digital = i%10;
-							temp[count++] = HEX_CHARACTERS[digital];
-							i = i/10;
-						}
-						//count--;	// 对于下标而言，这个count多加了一次
-						if (precision >= 0 && precision < count) {
-							count = precision - 1;
-						}else{
-							count--;
-						}
-						if (width > count) {
-							for (int i = 0; i < width - count; i++) {
-								out[index++] = ' '; // 右对齐
-							}
-						}
-						while(count>=0){
-							out[index++] = temp[count--];
-						}
-					}
-					break;
-				}
-				case 'c': {
-					char temp = (char)va_arg(ap,int);
-					if (width > 1) {
-						for (int i = 0; i < width - 1; i++) {
-							out[index++] = ' '; // 右对齐
-						}
-					}
-					out[index++] = temp;
-					break;
-				}
-				case 'x': {
-					int x = va_arg(ap, int);
-					char temp[32]; 
-					int count = 0,digital = 0;
-					while(x>0){
-						digital = x%16;
-						temp[count++] = HEX_CHARACTERS[digital];
-						x = x/16;
-					}
-					// 处理宽度和精度
-					// count和precision代表的是数字的位数，但是后续需要使用下标，所以需要减1
-					if (precision >= 0 && precision < count) {
-						count = precision - 1;
-					}else{
-						count--;
-					}
-					if (width > count) {
-						for (int i = 0; i < width - count; i++) {
-							out[index++] = ' '; // 右对齐
-						}
-					}
-					while(count>=0){
-						out[index++] = temp[count--];
-					}
-                    break;
-				}
-				default:{
-					out[index++] = *fmt;
-					break;
-				}
-					
-			}
-		} 
-		fmt++;
-	}
-	out[index] = '\0';
-
-	return index;
+  out[j] = '\0';
+  return j;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-	//panic("Not implemented");
+  va_list valist;
+  va_start(valist, fmt);
 
-	char *start = out;
-	va_list args;
-	// 解析...中的参数
-	va_start(args, fmt);
-	
-	vsprintf(out, fmt, args);
-	
-	va_end(args);
-	return out - start;
+  int res = vsprintf(out ,fmt, valist);
+  va_end(valist);
+  return res;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
