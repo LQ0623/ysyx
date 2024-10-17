2a9f26a14aff48d6897dc2c477191053c76f2b27
tracer-ysyx <tracer@ysyx.org>
2024-10-17 17:30:51 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:30:51 up  7:25,  1 user,  load average: 0.09, 0.12, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,20 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int w = 400;  // TODO: get the correct width
+  int h = 300;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = 400, .height = 300,
     .vmemsz = 0
   };
 }
@@ -22,4 +30,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
   status->ready = true;
+}
+
+void __am_gpu_memcpy(AM_GPU_MEMCPY_T *memcpy){
+  memcpy->size = inl(FB_ADDR);
 }
