33b1e932081dfb77f0b74bf226435475fead8e4f
tracer-ysyx <tracer@ysyx.org>
2024-10-17 17:31:38 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:31:38 up  7:26,  1 user,  load average: 0.23, 0.16, 0.29

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,7 +5,7 @@
 
 void __am_gpu_init() {
   int i;
-  int w = 500;  // TODO: get the correct width
+  int w = 400;  // TODO: get the correct width
   int h = 300;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   
@@ -17,7 +17,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 500, .height = 300,
+    .width = 400, .height = 300,
     .vmemsz = 0
   };
 }
