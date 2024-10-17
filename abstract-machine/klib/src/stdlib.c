4347b5256155f786ea74d13861a31d54a0ae0bd0
tracer-ysyx <tracer@ysyx.org>
2024-10-17 16:27:13 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:13 up  6:22,  1 user,  load average: 2.43, 1.92, 1.18

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,7 +5,7 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 // 分配的内存的首地址
-// static void* malloc_addr = NULL;
+static void* malloc_addr = NULL;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
@@ -31,27 +31,27 @@ int atoi(const char* nptr) {
   return x;
 }
 
-// void *malloc(size_t size) {
-//   // On native, malloc() will be called during initializaion of C runtime.
-//   // Therefore do not call panic() here, else it will yield a dead recursion:
-//   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-//   // panic("Not implemented");
-//   if(malloc_addr == NULL){
-//     malloc_addr = (void*)ROUNDUP(heap.start, 4);
-//   }
-//   size_t align_size = (size_t)ROUNDUP(size, 4);
-//   void* assign_addr = malloc_addr;
-//   malloc_addr = malloc_addr + align_size;
-//   assert(malloc_addr < heap.end);
-//   for(int i = 0;i < align_size;i++){
-//     char* p = assign_addr + i;
-//     *p = '\0';
-//   }
+void *malloc(size_t size) {
+  // On native, malloc() will be called during initializaion of C runtime.
+  // Therefore do not call panic() here, else it will yield a dead recursion:
+  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+  // panic("Not implemented");
+  if(malloc_addr == NULL){
+    malloc_addr = (void*)ROUNDUP(heap.start, 4);
+  }
+  size_t align_size = (size_t)ROUNDUP(size, 4);
+  void* assign_addr = malloc_addr;
+  malloc_addr = malloc_addr + align_size;
+  assert(malloc_addr < heap.end);
+  for(int i = 0;i < align_size;i++){
+    char* p = assign_addr + i;
+    *p = '\0';
+  }
 
-// #endif
-//   return assign_addr;
-// }
+#endif
+  return assign_addr;
+}
 
 void free(void *ptr) {
 }
