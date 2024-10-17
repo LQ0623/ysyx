e7667010aa71501cb73ca922edbb20c2cb192294
tracer-ysyx <tracer@ysyx.org>
2024-10-17 18:12:26 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:12:26 up  8:07,  1 user,  load average: 0.33, 0.22, 0.14

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,21 +2,21 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
-#define W_ADDR    (VGACTL_ADDR + 28)
+#define W_ADDR    (VGACTL_ADDR + 8)
 #define H_ADDR    (VGACTL_ADDR + 12)
 #define X_ADDR    (VGACTL_ADDR + 16)
 #define Y_ADDR    (VGACTL_ADDR + 20)
 #define PIXELS_ADDR (VGACTL_ADDR + 24)
 
 void __am_gpu_init() {
-  // int i;
-  // int w = 400;  // TODO: get the correct width
-  // int h = 300;  // TODO: get the correct height
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  int i;
+  int w = 400;  // TODO: get the correct width
+  int h = 300;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   
-  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
 
-  // outl(SYNC_ADDR, 1);
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -30,10 +30,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
-    outl(W_ADDR, ctl->w);
-    outl(H_ADDR, ctl->h);
-    outl(X_ADDR, ctl->x);
-    outl(Y_ADDR, ctl->y);
+    // outl(W_ADDR, ctl->w);
+    // outl(H_ADDR, ctl->h);
+    // outl(X_ADDR, ctl->x);
+    // outl(Y_ADDR, ctl->y);
     // outl(PIXELS_ADDR, ctl->pixels);
   }
 }
