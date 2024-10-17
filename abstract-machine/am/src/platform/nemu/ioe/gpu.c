b0e2ec23770383f91b67e7f41cd1fcdb52fc21ca
tracer-ysyx <tracer@ysyx.org>
2024-10-17 21:31:01 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:31:01 up 11:26,  1 user,  load average: 0.18, 0.39, 0.56

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -48,8 +48,3 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
   status->ready = true;
 }
-
-void __am_gpu_memcpy(AM_GPU_MEMCPY_T *memcpy){
-  uint32_t all_size = inl(VGACTL_ADDR);
-  memcpy->size = all_size;
-}
