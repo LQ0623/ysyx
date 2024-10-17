4afc45a7f30bdf4bb2c7b96ff49047edd37f22dc
tracer-ysyx <tracer@ysyx.org>
2024-10-17 21:07:17 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:07:17 up 11:02,  1 user,  load average: 0.79, 0.68, 0.50

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -79,8 +79,7 @@ void vga_update_screen() {
   // uint32_t sync = vgactl_port_base[1];
   if(1 == sync){
     update_screen();
-    vaddr_t addr = 0xa0000100 + 4;
-    vaddr_write(addr, 4, 0);
+    vaddr_write(0xa0000100+4, 4, 0);
     // vgactl_port_base[1] = 0;
   }
 }
