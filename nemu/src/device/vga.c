425b84c73fc1f24a69c8e06e0bffc60649745ce1
tracer-ysyx <tracer@ysyx.org>
2024-10-17 20:58:39 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:58:39 up 10:53,  1 user,  load average: 0.25, 0.33, 0.34

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -75,12 +75,10 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
-  // uint32_t sync = vaddr_read(CONFIG_FB_ADDR+4, 4);
-  uint32_t sync = vgactl_port_base[1];
+  uint32_t sync = vaddr_read(0xa1000000+4, 4);
   if(1 == sync){
     update_screen();
-    // vaddr_write(CONFIG_FB_ADDR+4, 4, 0);
-    vgactl_port_base[1] = 0;
+    vaddr_write(0xa1000000+4, 4, 0);
   }
 }
 
