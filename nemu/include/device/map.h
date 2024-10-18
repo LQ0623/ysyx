c5768dc0f1f3caa2db31eddfaddc3088c9925369
tracer-ysyx <tracer@ysyx.org>
2024-10-18 17:11:07 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:11:07 up  2:32,  1 user,  load average: 0.09, 0.15, 0.21

--- nemu/include/device/map.h
+++ nemu/include/device/map.h
@@ -38,6 +38,7 @@ static inline int find_mapid_by_addr(IOMap *maps, int size, paddr_t addr) {
   int i;
   for (i = 0; i < size; i ++) {
     if (map_inside(maps + i, addr)) {
+      Log("step in find_mapid_by_addr");
       difftest_skip_ref();
       return i;
     }
