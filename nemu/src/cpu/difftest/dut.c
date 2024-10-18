c36fedda46d63f9c57edc6e8fd3903fe318b1df4
tracer-ysyx <tracer@ysyx.org>
2024-10-18 17:03:58 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:58 up  2:25,  1 user,  load average: 0.17, 0.25, 0.26

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -34,7 +34,7 @@ static int skip_dut_nr_inst = 0;
 // this is used to let ref skip instructions which
 // can not produce consistent behavior with NEMU
 void difftest_skip_ref() {
-  panic("step in difftest_skip_ref");
+  Log("step in difftest_skip_ref");
   is_skip_ref = true;
   // If such an instruction is one of the instruction packing in QEMU
   // (see below), we end the process of catching up with QEMU's pc to
