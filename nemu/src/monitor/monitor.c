b955a26058a79e48f699f1c405612355574fe2dd
tracer-ysyx <tracer@ysyx.org>
2024-10-19 10:02:47 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:02:47 up  1:31,  1 user,  load average: 0.46, 0.25, 0.20

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -132,7 +132,7 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
-  Log("%s",diff_so_file);
+  Log("asdadadasdasdasdsadasdsadasdasdsad %s",diff_so_file);
 
   /* Initialize the simple debugger. */
   init_sdb();
