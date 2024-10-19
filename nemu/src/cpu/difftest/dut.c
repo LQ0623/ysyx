c32c36149d7e4351a46554cff9b44b39aa7b96c7
tracer-ysyx <tracer@ysyx.org>
2024-10-19 13:07:32 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:07:32 up  4:36,  1 user,  load average: 0.08, 0.02, 0.02

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -121,7 +121,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
     // Log("skip count is %d,is_skip_ref is %d\n",count++, is_skip_ref);
     // to skip the checking of an instruction, just copy the reg state to reference design
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
-    is_skip_ref = false;
+    //is_skip_ref = false;
     return;
   }
 
