7a3d41d7024106f9f0ab429e5a6a2b0b7331a571
tracer-ysyx <tracer@ysyx.org>
2024-10-19 09:22:12 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:22:12 up 50 min,  1 user,  load average: 0.36, 0.18, 0.15

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -37,7 +37,7 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
   assert(0);
   int i = 0;
   struct diff_context_t *dut_state = (struct diff_context_t *)dut;
-  if(direction == DIFFTEST_TO_DUT){
+  if(direction == DIFFTEST_TO_REF){
     for(i = 0;i < NR_GPR ; i++){
       cpu.gpr[i] = dut_state->gpr[i];
     }
