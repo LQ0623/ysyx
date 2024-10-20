926f6926d43c5b10e43f25d6bfdad8ca1733aaa3
tracer-ysyx <tracer@ysyx.org>
2024-10-20 16:33:14 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:14 up  2:09,  1 user,  load average: 0.24, 0.65, 1.26

--- nemu/include/macro.h
+++ nemu/include/macro.h
@@ -99,16 +99,12 @@
 
 // for AM IOE
 #define io_read(reg) \
-  ({ difftest_skip_ref();\
-    reg##_T __io_param; \
+  ({ reg##_T __io_param; \
     ioe_read(reg, &__io_param); \
-    __io_param;\
-    difftest_skip_ref(); })
+    __io_param; })
 
 #define io_write(reg, ...) \
-  ({ difftest_skip_ref();\
-    reg##_T __io_param = (reg##_T) { __VA_ARGS__ }; \
-    ioe_write(reg, &__io_param);\
-    difftest_skip_ref(); })
+  ({ reg##_T __io_param = (reg##_T) { __VA_ARGS__ }; \
+    ioe_write(reg, &__io_param); })
 
 #endif
