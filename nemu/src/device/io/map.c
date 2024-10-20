e0105a71c269e5803e4ed42e4d21f658f880e4f7
tracer-ysyx <tracer@ysyx.org>
2024-10-20 16:35:11 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:35:11 up  2:10,  1 user,  load average: 0.73, 0.64, 1.18

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -69,11 +69,11 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 }
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
+  difftest_skip_ref();
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
-  difftest_skip_ref();
   invoke_callback(map->callback, offset, len, true);
   difftest_skip_ref();
 #ifdef CONFIG_DTRACE
