a2968f6daf5adcea242bcd8517bfdc147db4e991
tracer-ysyx <tracer@ysyx.org>
2024-10-22 13:31:10 +0800

    > sim RTL ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:31:10 up  3:40,  1 user,  load average: 0.42, 0.32, 0.31

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -15,8 +15,8 @@ uint32_t guest_to_host(uint32_t addr);
 uint32_t pmem_read(uint32_t *memory, uint32_t vaddr);
 
 void single_cycle(){
-    top->clk = 0;top->eval();
-    top->clk = 1;top->eval();
+    top->clk = 0;top->eval();top->eval();
+    top->clk = 1;top->eval();top->eval();
 }
 
 static void reset(int n){
