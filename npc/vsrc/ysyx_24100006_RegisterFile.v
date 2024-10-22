1cc911dfdb9877f1e228de74484010aa2a773035
tracer-ysyx <tracer@ysyx.org>
2024-10-22 13:17:33 +0800

    > sim RTL ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:17:33 up  3:26,  1 user,  load average: 1.21, 0.53, 0.25

--- npc/vsrc/ysyx_24100006_RegisterFile.v
+++ npc/vsrc/ysyx_24100006_RegisterFile.v
@@ -18,7 +18,7 @@ module ysyx_24100006_RegisterFile #(ADDR_WIDTH = 5, DATA_WIDTH = 32) (
   always@(posedge clk)begin
     if(!reset)begin
       for(i = 0;i < 2**ADDR_WIDTH;i = i + 1)begin
-        rf[i] = 32'b0;
+        rf[i] <= 32'b0;
       end
     end
   end
