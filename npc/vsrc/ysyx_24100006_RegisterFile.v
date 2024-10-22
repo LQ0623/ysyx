52d7478454f820be7853e9763d3928893319d32e
tracer-ysyx <tracer@ysyx.org>
2024-10-22 13:17:22 +0800

    > sim RTL ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:17:22 up  3:26,  1 user,  load average: 1.15, 0.50, 0.23

--- npc/vsrc/ysyx_24100006_RegisterFile.v
+++ npc/vsrc/ysyx_24100006_RegisterFile.v
@@ -1,5 +1,6 @@
 module ysyx_24100006_RegisterFile #(ADDR_WIDTH = 5, DATA_WIDTH = 32) (
   input clk,
+  input reset,
   input [DATA_WIDTH-1:0] wdata,
   input [ADDR_WIDTH-1:0] waddr,
   input wen,
@@ -11,6 +12,15 @@ module ysyx_24100006_RegisterFile #(ADDR_WIDTH = 5, DATA_WIDTH = 32) (
   reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0];
   always @(posedge clk) begin
     if (wen) rf[waddr] <= wdata;
+  end
+
+  integer i;
+  always@(posedge clk)begin
+    if(!reset)begin
+      for(i = 0;i < 2**ADDR_WIDTH;i = i + 1)begin
+        rf[i] = 32'b0;
+      end
+    end
   end
 
   assign rs1_data = rf[rs1];
