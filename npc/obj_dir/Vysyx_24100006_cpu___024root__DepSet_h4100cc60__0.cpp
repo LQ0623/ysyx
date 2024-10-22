ccddeb92f549185690cce129d532fa4b0d8d4c82
tracer-ysyx <tracer@ysyx.org>
2024-10-22 13:27:21 +0800

    > sim RTL ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:27:21 up  3:36,  1 user,  load average: 0.20, 0.34, 0.32

---
+++ npc/obj_dir/Vysyx_24100006_cpu___024root__DepSet_h4100cc60__0.cpp
@@ -0,0 +1,204 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vysyx_24100006_cpu.h for the primary calling header
+
+#include "verilated.h"
+
+#include "Vysyx_24100006_cpu___024root.h"
+
+VL_INLINE_OPT void Vysyx_24100006_cpu___024root___ico_sequent__TOP__0(Vysyx_24100006_cpu___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vysyx_24100006_cpu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vysyx_24100006_cpu___024root___ico_sequent__TOP__0\n"); );
+    // Body
+    vlSelf->result = (vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf
+                      [(0x1fU & (vlSelf->instruction 
+                                 >> 0xfU))] + (((- (IData)(
+                                                           (vlSelf->instruction 
+                                                            >> 0x1fU))) 
+                                                << 0xcU) 
+                                               | (vlSelf->instruction 
+                                                  >> 0x14U)));
+}
+
+void Vysyx_24100006_cpu___024root___eval_ico(Vysyx_24100006_cpu___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vysyx_24100006_cpu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vysyx_24100006_cpu___024root___eval_ico\n"); );
+    // Body
+    if (vlSelf->__VicoTriggered.at(0U)) {
+        Vysyx_24100006_cpu___024root___ico_sequent__TOP__0(vlSelf);
+    }
+}
+
+void Vysyx_24100006_cpu___024root___eval_act(Vysyx_24100006_cpu___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vysyx_24100006_cpu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vysyx_24100006_cpu___024root___eval_act\n"); );
+}
+
+VL_INLINE_OPT void Vysyx_24100006_cpu___024root___nba_sequent__TOP__0(Vysyx_24100006_cpu___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vysyx_24100006_cpu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vysyx_24100006_cpu___024root___nba_sequent__TOP__0\n"); );
+    // Init
+    CData/*0:0*/ __Vdlyvset__ysyx_24100006_cpu__DOT__registerfile__DOT__rf__v0;
+    __Vdlyvset__ysyx_24100006_cpu__DOT__registerfile__DOT__rf__v0 = 0;
+    // Body
+    __Vdlyvset__ysyx_24100006_cpu__DOT__registerfile__DOT__rf__v0 = 0U;
+    if (vlSelf->reset) {
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__i = 0x20U;
+        __Vdlyvset__ysyx_24100006_cpu__DOT__registerfile__DOT__rf__v0 = 1U;
+        vlSelf->ysyx_24100006_cpu__DOT__pc = 0x80000000U;
+    } else {
+        vlSelf->ysyx_24100006_cpu__DOT__pc = vlSelf->ysyx_24100006_cpu__DOT__npc;
+    }
+    if (__Vdlyvset__ysyx_24100006_cpu__DOT__registerfile__DOT__rf__v0) {
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[1U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[2U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[3U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[4U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[5U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[6U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[7U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[8U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[9U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0xaU] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0xbU] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0xcU] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0xdU] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0xeU] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0xfU] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x10U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x11U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x12U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x13U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x14U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x15U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x16U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x17U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x18U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x19U] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x1aU] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x1bU] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x1cU] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x1dU] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x1eU] = 0U;
+        vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf[0x1fU] = 0U;
+    }
+    vlSelf->result = (vlSelf->ysyx_24100006_cpu__DOT__registerfile__DOT__rf
+                      [(0x1fU & (vlSelf->instruction 
+                                 >> 0xfU))] + (((- (IData)(
+                                                           (vlSelf->instruction 
+                                                            >> 0x1fU))) 
+                                                << 0xcU) 
+                                               | (vlSelf->instruction 
+                                                  >> 0x14U)));
+    vlSelf->x_pc = vlSelf->ysyx_24100006_cpu__DOT__pc;
+    vlSelf->ysyx_24100006_cpu__DOT__npc = ((IData)(4U) 
+                                           + vlSelf->ysyx_24100006_cpu__DOT__pc);
+}
+
+void Vysyx_24100006_cpu___024root___eval_nba(Vysyx_24100006_cpu___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vysyx_24100006_cpu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vysyx_24100006_cpu___024root___eval_nba\n"); );
+    // Body
+    if (vlSelf->__VnbaTriggered.at(0U)) {
+        Vysyx_24100006_cpu___024root___nba_sequent__TOP__0(vlSelf);
+        vlSelf->__Vm_traceActivity[1U] = 1U;
+    }
+}
+
+void Vysyx_24100006_cpu___024root___eval_triggers__ico(Vysyx_24100006_cpu___024root* vlSelf);
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vysyx_24100006_cpu___024root___dump_triggers__ico(Vysyx_24100006_cpu___024root* vlSelf);
+#endif  // VL_DEBUG
+void Vysyx_24100006_cpu___024root___eval_triggers__act(Vysyx_24100006_cpu___024root* vlSelf);
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vysyx_24100006_cpu___024root___dump_triggers__act(Vysyx_24100006_cpu___024root* vlSelf);
+#endif  // VL_DEBUG
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vysyx_24100006_cpu___024root___dump_triggers__nba(Vysyx_24100006_cpu___024root* vlSelf);
+#endif  // VL_DEBUG
+
+void Vysyx_24100006_cpu___024root___eval(Vysyx_24100006_cpu___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vysyx_24100006_cpu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vysyx_24100006_cpu___024root___eval\n"); );
+    // Init
+    CData/*0:0*/ __VicoContinue;
+    VlTriggerVec<1> __VpreTriggered;
+    IData/*31:0*/ __VnbaIterCount;
+    CData/*0:0*/ __VnbaContinue;
+    // Body
+    vlSelf->__VicoIterCount = 0U;
+    __VicoContinue = 1U;
+    while (__VicoContinue) {
+        __VicoContinue = 0U;
+        Vysyx_24100006_cpu___024root___eval_triggers__ico(vlSelf);
+        if (vlSelf->__VicoTriggered.any()) {
+            __VicoContinue = 1U;
+            if (VL_UNLIKELY((0x64U < vlSelf->__VicoIterCount))) {
+#ifdef VL_DEBUG
+                Vysyx_24100006_cpu___024root___dump_triggers__ico(vlSelf);
+#endif
+                VL_FATAL_MT("/home/lq/ysyx-workbench/npc/vsrc/ysyx_24100006_cpu.v", 1, "", "Input combinational region did not converge.");
+            }
+            vlSelf->__VicoIterCount = ((IData)(1U) 
+                                       + vlSelf->__VicoIterCount);
+            Vysyx_24100006_cpu___024root___eval_ico(vlSelf);
+        }
+    }
+    __VnbaIterCount = 0U;
+    __VnbaContinue = 1U;
+    while (__VnbaContinue) {
+        __VnbaContinue = 0U;
+        vlSelf->__VnbaTriggered.clear();
+        vlSelf->__VactIterCount = 0U;
+        vlSelf->__VactContinue = 1U;
+        while (vlSelf->__VactContinue) {
+            vlSelf->__VactContinue = 0U;
+            Vysyx_24100006_cpu___024root___eval_triggers__act(vlSelf);
+            if (vlSelf->__VactTriggered.any()) {
+                vlSelf->__VactContinue = 1U;
+                if (VL_UNLIKELY((0x64U < vlSelf->__VactIterCount))) {
+#ifdef VL_DEBUG
+                    Vysyx_24100006_cpu___024root___dump_triggers__act(vlSelf);
+#endif
+                    VL_FATAL_MT("/home/lq/ysyx-workbench/npc/vsrc/ysyx_24100006_cpu.v", 1, "", "Active region did not converge.");
+                }
+                vlSelf->__VactIterCount = ((IData)(1U) 
+                                           + vlSelf->__VactIterCount);
+                __VpreTriggered.andNot(vlSelf->__VactTriggered, vlSelf->__VnbaTriggered);
+                vlSelf->__VnbaTriggered.set(vlSelf->__VactTriggered);
+                Vysyx_24100006_cpu___024root___eval_act(vlSelf);
+            }
+        }
+        if (vlSelf->__VnbaTriggered.any()) {
+            __VnbaContinue = 1U;
+            if (VL_UNLIKELY((0x64U < __VnbaIterCount))) {
+#ifdef VL_DEBUG
+                Vysyx_24100006_cpu___024root___dump_triggers__nba(vlSelf);
+#endif
+                VL_FATAL_MT("/home/lq/ysyx-workbench/npc/vsrc/ysyx_24100006_cpu.v", 1, "", "NBA region did not converge.");
+            }
+            __VnbaIterCount = ((IData)(1U) + __VnbaIterCount);
+            Vysyx_24100006_cpu___024root___eval_nba(vlSelf);
+        }
+    }
+}
+
+#ifdef VL_DEBUG
+void Vysyx_24100006_cpu___024root___eval_debug_assertions(Vysyx_24100006_cpu___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vysyx_24100006_cpu__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vysyx_24100006_cpu___024root___eval_debug_assertions\n"); );
+    // Body
+    if (VL_UNLIKELY((vlSelf->clk & 0xfeU))) {
+        Verilated::overWidthError("clk");}
+    if (VL_UNLIKELY((vlSelf->reset & 0xfeU))) {
+        Verilated::overWidthError("reset");}
+}
+#endif  // VL_DEBUG
