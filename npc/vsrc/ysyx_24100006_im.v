eb4c797e8134f0532c82d40fd5878db158119206
tracer-ysyx <tracer@ysyx.org>
2024-10-23 15:03:50 +0800

    > sim RTL ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:03:50 up 43 min,  1 user,  load average: 0.73, 0.75, 0.44

--- npc/vsrc/ysyx_24100006_im.v
+++ npc/vsrc/ysyx_24100006_im.v
@@ -8,9 +8,13 @@ module ysyx_24100006_im(
 );
 
     reg [31:0] instructions[1023:0];
+    integer i;
     // initial 用于初始化
     initial begin
         $readmemh("/home/lq/ysyx-workbench/npc/vsrc/inst.txt",instructions);
+        for(i = 0;i<10;i = i+1)begin
+            $$display("instruction is %x",instructions[i]);
+        end
     end
 
     assign instruction = instructions[pc[11:2]];
