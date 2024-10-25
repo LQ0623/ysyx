7e3ba87b7d69fdea79a96b923f8d686a6664c0c1
tracer-ysyx <tracer@ysyx.org>
2024-10-25 21:23:51 +0800

    > sim RTL ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:51 up  4:21,  1 user,  load average: 0.27, 0.34, 0.38

--- npc/vsrc/IF/ysyx_24100006_pc.v
+++ npc/vsrc/IF/ysyx_24100006_pc.v
@@ -2,7 +2,7 @@ module ysyx_24100006_pc(
     input clk,
     input reset,
     input[31:0] npc,
-    output reg [31:0] pc
+    output[31:0] pc
 );
     ysyx_24100006_Reg #(32,32'h80000000) pc1(
 		.clk(clk),
