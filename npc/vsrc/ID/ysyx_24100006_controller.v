75e3714c8ff59c2075c248dff9df33316769fa40
tracer-ysyx <tracer@ysyx.org>
2024-10-25 20:48:33 +0800

    > sim RTL ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:48:33 up  3:45,  1 user,  load average: 0.13, 0.07, 0.14

--- npc/vsrc/ID/ysyx_24100006_controller.v
+++ npc/vsrc/ID/ysyx_24100006_controller.v
@@ -2,8 +2,68 @@
     输出一些控制信号
 */
 
-`include "ysyx_24100006_ctrl_define.v"
-`include "ysyx_24100006_inst_define.v"
+// `include "ysyx_24100006_ctrl_define.v"
+// `include "ysyx_24100006_inst_define.v"
+
+// 控制信号宏定义
+// REG_WRITE
+`define ysyx_24100006_REGW                  1
+`define ysyx_24100006_REGNW                 0
+// MEM_WRITE
+`define ysyx_24100006_MEMW                  1
+`define ysyx_24100006_MEMNW                 0
+// pc跳转是否加imm
+`define ysyx_24100006_NJUMP                 0
+`define ysyx_24100006_JAL                   1
+`define ysyx_24100006_JALR                  2
+`define ysyx_24100006_JBEQ                  3
+// 指令的imm的类型
+`define ysyx_24100006_I_TYPE_IMM            0
+`define ysyx_24100006_J_TYPE_IMM            1
+`define ysyx_24100006_S_TYPE_IMM            2
+`define ysyx_24100006_B_TYPE_IMM            3
+`define ysyx_24100006_U_TYPE_IMM            4
+// 操作
+`define ysyx_24100006_add_op                0
+`define ysyx_24100006_sub_op                1
+
+//ALU的源操作数
+//AluSrcA
+`define ysyx_24100006_A_PC                  1
+`define ysyx_24100006_A_RS                  0
+//AluSrcB
+`define ysyx_24100006_B_IMM                 1
+`define ysyx_24100006_B_RT                  0
+//写寄存器的内容
+`define ysyx_24100006_REG_IMM               0   // 写回寄存器的是符号扩展之后的立即数
+`define ysyx_24100006_REG_RESULT            1   // 写回寄存器的是alu计算的结果
+`define ysyx_24100006_REG_PC_PLUS_4         2   // 写回寄存器的是pc+4的结果
+
+
+//opcode
+`define ysyx_24100006_SYSTEM              7'b1110011
+`define ysyx_24100006_I_type              7'b0010011
+`define ysyx_24100006_R_type              7'b0110011
+`define ysyx_24100006_S_type              7'b0100011
+`define ysyx_24100006_B_type              7'b1100011
+`define ysyx_24100006_auipc               7'b0010111
+`define ysyx_24100006_lui                 7'b0110111
+`define ysyx_24100006_jal                 7'b1101111
+`define ysyx_24100006_jalr                7'b1100111
+//function
+  //ysyx_24100006_SYSTEM
+`define ysyx_24100006_ebreak              12'b000000000001
+
+  //ysyx_24100006_I_type
+`define ysyx_24100006_addi                3'b000
+  //ysyx_24100006_R_type
+`define ysyx_24100006_add_sub             3'b000
+`define ysyx_24100006_add                 7'b0000000
+`define ysyx_24100006_sub                 7'b0100000
+  //ysyx_24100006_S_type
+`define ysyx_24100006_sw                  3'b010
+  //ysyx_24100006_B_type
+`define ysyx_24100006_beq                 3'b000
 
 
 import "DPI-C" function void npc_trap ();
