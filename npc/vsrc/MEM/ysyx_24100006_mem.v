41f7cf9326ad1af3c8d6b29e0729b8f30dd1d4b8
tracer-ysyx <tracer@ysyx.org>
2024-10-25 21:05:59 +0800

    > sim RTL ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:05:58 up  4:03,  1 user,  load average: 0.33, 0.42, 0.32

--- npc/vsrc/MEM/ysyx_24100006_mem.v
+++ npc/vsrc/MEM/ysyx_24100006_mem.v
@@ -5,12 +5,12 @@ module ysyx_24100006_mem(
     input [31:0] wdata
 );
 
-    import "DPI-C" function int pmem_read(input int raddr);
-    import "DPI-C" function void pmem_write(input int waddr, input int wdata);
-    always@(posedge clk)begin
-        if(Mem_Write)begin
-            pmem_write(waddr,wdata);
-        end
-    end
+    // import "DPI-C" function int pmem_read(input int raddr);
+    // import "DPI-C" function void pmem_write(input int waddr, input int wdata);
+    // always@(posedge clk)begin
+    //     if(Mem_Write)begin
+    //         pmem_write(waddr,wdata);
+    //     end
+    // end
 
 endmodule
