f783a437ae4b4c6a5b3a57f0297271f11f3ffa70
tracer-ysyx <tracer@ysyx.org>
2024-10-26 16:07:14 +0800

    > sim RTL ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:07:14 up  5:59,  1 user,  load average: 0.94, 1.74, 1.81

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -22,7 +22,7 @@ void single_cycle(){
 extern "C"  void npc_trap() {
     // single_cycle();
     // single_cycle();
-    // tfp -> close();
+    tfp -> close();
     printf("asdadasdad\n");
     ebreak = 0;
 }
