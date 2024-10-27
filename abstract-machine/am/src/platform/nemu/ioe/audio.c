3f1a68216157a268f9950e7345eeae97246fa83a
tracer-ysyx <tracer@ysyx.org>
2024-10-27 14:59:24 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:59:24 up  3:23,  1 user,  load average: 0.21, 0.31, 0.27

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -12,6 +12,7 @@ void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
+  cfg->present = false;
   /*cfg->present = true;
   cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR); // Stream buffer*/
 }
@@ -23,6 +24,7 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
+  stat->count = 0;
   /*stat->count = inl(AUDIO_COUNT_ADDR);*/
 }
 
