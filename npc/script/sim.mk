33eac5b5b4b02e6ffe7e38737675ad85fd06e554
tracer-ysyx <tracer@ysyx.org>
2024-11-04 20:55:14 +0800

    > sim RTL ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:55:14 up 49 min,  1 user,  load average: 0.27, 0.35, 0.34

--- npc/script/sim.mk
+++ npc/script/sim.mk
@@ -1,5 +1,5 @@
 #testbench
-CTB = $(shell find $(abspath $(NPC_HOME)/csrc/tb) -name "*.c" -or -name "*.cc" -or -name "*.cpp")
+CTB = $(shell find $(abspath $(NPC_HOME)/csrc) -name "*.c" -or -name "*.cc" -or -name "*.cpp")
 
 
 NPC_EXEC := $(BIN) $(ARGS) $(IMG)
