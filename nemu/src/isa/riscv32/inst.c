5aa53eaf8fe9ff9528f3c7e10381b6a24913b751
tracer-ysyx <tracer@ysyx.org>
2024-12-01 17:45:42 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:45:42 up  6:32,  1 user,  load average: 0.24, 0.23, 0.32

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -149,6 +149,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 001 ????? 01100 11", mulh   , Rt, int64_t a = (int32_t)src1; int64_t b = (int32_t)src2;int64_t temp = a * b; R(rd) = BITS(temp, 63, 32));
   INSTPAT("0000001 ????? ????? 010 ????? 01100 11", mulhsu , Rt, int64_t a = (int32_t)src1; uint64_t b = src2;int64_t temp = a * b; R(rd) = BITS(temp, 63, 32));
   INSTPAT("0000001 ????? ????? 011 ????? 01100 11", mulhu  , Rt, uint64_t a = src1; uint64_t b = src2;uint64_t temp = a * b; R(rd) = BITS(temp, 63, 32));
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , Rt, s->dnpc = SR(MEPC));
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(1, s->pc)); // 环境调用
