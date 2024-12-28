1e26374d9e0d79b29881375f7fe63360e0579057
tracer-ysyx <tracer@ysyx.org>
2024-12-28 15:36:16 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:36:16 up  4:16,  1 user,  load average: 0.04, 0.01, 0.04

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -20,7 +20,7 @@ Context* __am_irq_handle(Context *c) {
     }
 
     c = user_handler(ev, c);
-    printf("mcause:%tu\tmstatus:%p\tmepc:%p\n",(void*)c->mcause,(void*)c->mstatus,(void*)c->mepc);
+    //printf("\nmcause:%tu\tmstatus:%p\tmepc:%p\n",(void*)c->mcause,(void*)c->mstatus,(void*)c->mepc);
     assert(c != NULL);
   }
 
