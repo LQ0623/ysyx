3be5f6e264562b336d356046ad7df29b22032dee
tracer-ysyx <tracer@ysyx.org>
2025-01-06 21:29:41 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:29:41 up  9:40,  1 user,  load average: 0.24, 0.36, 0.41

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,23 +24,33 @@ extern uint8_t ramdisk_end;
 
 // 这里不是通过fopen打开文件进行操作，而是通过直接读取文件来进行操作
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf_Ehdr ehdr;
-  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-  // check valid elf
-  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
 
-  Elf_Phdr phdr[ehdr.e_phnum];
-  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
-  for (int i = 0; i < ehdr.e_phnum; i++) {
+  printf("%d\n",((&ramdisk_end) - (&ramdisk_start)));
+  // 检查ELF文件头
+  Elf_Ehdr elf_header;
+  ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
+  
+  // check valid elf
+  assert((*(uint32_t *)elf_header.e_ident == 0x464c457f));
+
+  // 获取program header的信息
+  Elf_Phdr *phdr = (Elf_Phdr*)malloc(elf_header.e_phnum * sizeof(Elf_Phdr));
+
+  ramdisk_read(phdr, elf_header.e_phoff, sizeof(Elf_Phdr)*elf_header.e_phnum);
+  //循环遍历LOAD类型并加载到内存中
+  //加载区间     [VirtAddr, VirtAddr + MemSiz)
+  //.bss清零区间 [VirtAddr + FileSiz, VirtAddr + MemSiz)
+  int program_headers_num = elf_header.e_phnum;
+  for(int i = 0;i < program_headers_num;i++){
     if (phdr[i].p_type == PT_LOAD) {
       ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
       // set .bss with zeros
       memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
     }
   }
-  return ehdr.e_entry;
-}
 
+  return elf_header.e_entry;
+}
 
 
 void naive_uload(PCB *pcb, const char *filename) {;
