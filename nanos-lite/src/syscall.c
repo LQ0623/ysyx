0ba5dac7f61906bb307098743d6587dd5f66a888
tracer-ysyx <tracer@ysyx.org>
2025-01-07 10:24:55 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:24:55 up 13:52,  1 user,  load average: 1.91, 1.40, 0.95

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,16 +2,15 @@
 #include "syscall.h"
 void do_syscall(Context *c) {
   uintptr_t a[4];
-  // uintptr_t ret_value;
   a[0]          = c->GPR1;
   a[1]          = c->GPR2;
   a[2]          = c->GPR3;
   a[3]          = c->GPR4;
-  // ret_value    = c->GPRx;
 
   switch (a[0]) {
     case SYS_yield:
       Log("sys_yield");
+      c->GPRx = 0;
       yield();
       break;
     case SYS_exit:
