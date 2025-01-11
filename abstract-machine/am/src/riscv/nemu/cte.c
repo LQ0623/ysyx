575cc3a09fb506091bc101994afe212f857b8806
tracer-ysyx <tracer@ysyx.org>
2025-01-11 22:19:18 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:19:18 up 55 min,  1 user,  load average: 0.44, 0.53, 0.58

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -59,7 +59,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   ctx->mstatus=0x1800;
   ctx->mcause=11;
   ctx->gpr[10 /*a0*/]=(uintptr_t)arg;
-  ctx->gpr[2 /*sp*/]=(uintptr_t)(kstack.end - sizeof(Context));
+  // ctx->gpr[2 /*sp*/]=(uintptr_t)(kstack.end - sizeof(Context));
   return ctx;
 }
 
