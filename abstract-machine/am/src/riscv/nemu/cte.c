db5cb51da70da0515e74a1d99d51637365ef900d
tracer-ysyx <tracer@ysyx.org>
2025-01-12 11:29:07 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:29:07 up  5:58,  1 user,  load average: 1.18, 0.78, 0.52

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -46,7 +46,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   // 需要在kstack的底部创建一个以entry为入口的上下文结构
   // 现在有了CP，只需要在CP的底部创建一个Context就行
-  Context *c = (Context *)(kstack.end - 1);
+  Context *c = (Context *)(kstack.end - sizeof(Context) - 1);
   c->mepc = (uintptr_t)entry;
   c->mstatus = 0x1800;
   // 在riscv32中，对于整数和指针类型的参数，前8个参数通常通过x10 - x17这8个通用寄存器来传递
