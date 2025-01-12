8fd3998a3d2cdda6ce3b9c6bb7b698d4cbbe4eaf
tracer-ysyx <tracer@ysyx.org>
2025-01-12 15:00:46 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:00:46 up  9:29,  1 user,  load average: 0.72, 0.54, 0.39

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -43,25 +43,25 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
  * entry是内核线程的入口 
  * arg则是内核线程的参数
  */
-// Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-//   // 需要在kstack的底部创建一个以entry为入口的上下文结构
-//   // 现在有了CP，只需要在CP的底部创建一个Context就行
-//   Context *c = (Context *)kstack.end - 1;
-//   c->mepc = (uintptr_t)entry;
-//   c->mstatus = 0x1800;
-//   // 在riscv32中，对于整数和指针类型的参数，前8个参数通常通过x10 - x17这8个通用寄存器来传递
-//   c->gpr[10] = (uintptr_t)arg;
-//   return c;
-// }
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  Context *ctx = kstack.end - sizeof(Context);
-  ctx->mepc=(uintptr_t)entry;
-  ctx->mstatus=0x1800;
-  // ctx->mcause=11;
-  ctx->gpr[10 /*a0*/]=(uintptr_t)arg;
-  ctx->gpr[2 /*sp*/]=(uintptr_t)(kstack.end - sizeof(Context));
-  return ctx;
+  // 需要在kstack的底部创建一个以entry为入口的上下文结构
+  // 现在有了CP，只需要在CP的底部创建一个Context就行
+  Context *c = (Context *)kstack.end - 1;
+  c->mepc = (uintptr_t)entry;
+  c->mstatus = 0x1800;
+  // 在riscv32中，对于整数和指针类型的参数，前8个参数通常通过x10 - x17这8个通用寄存器来传递
+  c->gpr[10] = (uintptr_t)arg;
+  return c;
 }
+// Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
+//   Context *ctx = kstack.end - sizeof(Context);
+//   ctx->mepc=(uintptr_t)entry;
+//   ctx->mstatus=0x1800;
+//   ctx->mcause=11;
+//   ctx->gpr[10 /*a0*/]=(uintptr_t)arg;
+//   ctx->gpr[2 /*sp*/]=(uintptr_t)(kstack.end - sizeof(Context));
+//   return ctx;
+// }
 
 
 void yield() {
