5d10bda440aa33c3c0174d6d082091042d9b6b4c
tracer-ysyx <tracer@ysyx.org>
2025-05-06 14:13:15 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:13:15 up 21:14,  1 user,  load average: 0.40, 0.74, 0.78

--- nemu/src/ysyxSoC/memory/memory.c
+++ nemu/src/ysyxSoC/memory/memory.c
@@ -108,9 +108,9 @@ word_t uart_io_read(paddr_t addr, int len){
 
 void uart_io_write(paddr_t addr, int len, word_t data){
     assert(len ==1);
-    if(addr == UART_REG_RB){
-        putchar(data);
-    }
+    // if(addr == UART_REG_RB){
+    //     putchar(data);
+    // }
 }
 
 word_t socDev_read(paddr_t addr,int len){
