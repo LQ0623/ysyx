1a37543c2c0bfb0132f43399e5ebd90762516915
tracer-ysyx <tracer@ysyx.org>
2025-06-21 22:03:05 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:03:05 up  2:03,  1 user,  load average: 1.62, 1.64, 1.73

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -65,7 +65,7 @@ void init_mem() {
 #endif
   IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE));
   Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
-  // init_soc();
+  init_soc();
 }
 
 word_t paddr_read(paddr_t addr, int len) {
