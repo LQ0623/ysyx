5f7be261c00610578339211b50dd6bf6f9ee75f6
tracer-ysyx <tracer@ysyx.org>
2025-06-24 13:21:13 +0800

    > compile NEMU ysyx_24100006 罗权 Linux lq-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:21:13 up 1 day,  5:49,  1 user,  load average: 0.67, 0.51, 0.42

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -78,8 +78,6 @@ __EXPORT void difftest_init(int port) {
 
   void init_soc();
   init_soc();
-  // void init_mem();
-  // init_mem();
   /* Perform ISA dependent initialization. */
   init_isa();
 }
